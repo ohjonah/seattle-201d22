786e5a9d298e3f9f4f4d706afdd94cc7b9b96410
bnates <brian.nations@codefellows.com>
2017-05-09 12:52:48 -0700

    added lab req/lecture code

---
+++ 02-intro_to_html_css_js/lecture/day-2-demo/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var kids = prompt('how many kids does brian have?');
+console.log('number of kids:', kids);
+
+var hats = prompt('how many hats does brian own?');
+
+/* uppercase and lowercase string methods:
+  var str = 'Some String';
+  str.toUpperCase();
+  >> SOME STRING
+
+  str.toLowerCase();
+  >> some string
+*/
+
+if (kids === '3' || kids === '4') {
+  // do stuff here
+}
+
+if (kids === '3' && hats === '2') {
+  // do more stuff here
+}
