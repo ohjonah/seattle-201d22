e832cc2a8531a4f28dc8a1c02a96baf9d71ae19c
ohjonah <ohjonah@gmail.com>
2017-05-12 16:21:42 -0700

    completed function three

--- 05-images_color_text_ui_ux/lab/app.js
+++ 05-images_color_text_ui_ux/lab/app.js
@@ -56,11 +56,23 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
+  var myFirstSum = sum(a, b);
+  var mySecondSum = sum(myFirstSum[0], c);
+  var mySum = mySecondSum[0];
+  var mySumMsg = a + ' and ' + b + ' and ' + c + ' sum to ' + mySum + '.';
 
+  var myFirstProduct = multiply(a, b);
+  var mySecondProduct = multiply(myFirstProduct[0], c);
+  var myProduct = mySecondProduct[0];
+  var myProductMsg = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + myProduct + '.';
+
+  return [mySum, myProduct, mySumMsg, myProductMsg];
 }
 
+sumAndMultiply(4,7,5);
+
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
